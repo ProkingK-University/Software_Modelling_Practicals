6e66e786cf3028c58d2e16965f0666538d680e50
Joshua Wereley <joshuawereley@icloud.com>
2023-08-02 18:24:49 +0200

    Removed to functions that need to be implemented into cargoShip class

--- Practical_1/Task_1/Ship.h
+++ Practical_1/Task_1/Ship.h
@@ -15,9 +15,6 @@ public:
     void setShipId(int shipId);
     void setShipName(std::string shipName);
 
-    void insertShipPrototype(Ship* ship);
-    void deleteShipPrototype(Ship* ship);
-
 protected:
     virtual Ship* clone() = 0;
 };
