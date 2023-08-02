b2b348dde3b346ed21978b0b5d16e242f7d3d401
Ashley Kapaso <ashleykapaso@gmail.com>
2023-08-02 14:12:01 +0200

    add cargo ship function shells

---
+++ Practical_1/Task_1/CargoShip.cpp
@@ -0,0 +1,61 @@
+#include "CargoShip.h"
+
+CargoShip::CargoShip(int capacity)
+{
+
+}
+
+int CargoShip::getCapacity()
+{
+
+}
+
+int CargoShip::getCurrentLoad()
+{
+
+}
+
+std::string* CargoShip::getItems()
+{
+
+}
+
+void CargoShip::setCapacity(int capacity)
+{
+
+}
+
+void CargoShip::setItems(std::string items)
+{
+
+}
+
+void CargoShip::setCurrentLoad(int currentLoad)
+{
+
+}
+
+std::string CargoShip::toString()
+{
+
+}
+
+Ship* CargoShip::clone()
+{
+
+}
+
+CargoMemento CargoShip::save(int saveId)
+{
+
+}
+
+void CargoShip::restore(CargoMemento restorePoint)
+{
+
+}
+
+CargoShip::~CargoShip()
+{
+    
+}
