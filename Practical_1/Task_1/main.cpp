ba19756642d59ffc5094bea0156eb0d2e334981a
Joshua Wereley <joshuawereley@icloud.com>
2023-08-03 09:44:19 +0200

    Updated main.cpp
    Added the different functions that we can use to test the correctness of our Ship, CargoShip and CargoMemento code

--- Practical_1/Task_1/main.cpp
+++ Practical_1/Task_1/main.cpp
@@ -2,7 +2,101 @@
 #include "CargoShip.h"
 #include "Ship.h"
 
-int main() {
-    
+void protoTypeTesting();
+void mementoTesting();
+void finalTesting();
+void circularReferenceTesting();
+void cloningUnitializedObjectsTesting();
+void protoTypeRegistrationAndManagement();
+void deepCopyTesting();
+void invalidStateTesting();
+void partialRestorationTesting();
+
+int main() 
+{
+    protoTypeTesting();
+    mementoTesting();
+    finalTesting();
     return 0;
+}
+
+void protoTypeTesting() 
+{
+    circularReferenceTesting();
+    cloningUnitializedObjectsTesting();
+    protoTypeRegistrationAndManagement();
+}
+
+/*
+Test the pattern with objects having circular references (object A references
+object B, and object B references object A). Ensure that the prototype
+cloning process handles circular references gracefully without entering an
+infinite loop.
+*/
+void circularReferenceTesting() 
+{
+    
+}
+
+/*
+Attempt to clone objects that have not been fully initialized or contain null
+values. The prototype should create a valid copy without errors or issues.
+*/
+void cloningUnitializedObjectsTesting() 
+{
+
+}
+
+
+/*
+Test the registration and management of prototype objects in the prototype
+manager (if used). Ensure that prototypes are registered correctly and that
+the prototype manager can handle concurrent access and modification to the
+prototype registry.
+*/
+void protoTypeRegistrationAndManagement() 
+{
+
+}
+
+void mementoTesting() 
+{
+    deepCopyTesting();
+    invalidStateTesting();
+    partialRestorationTesting();
+}
+
+/*
+Check if the Memento is performing a deep copy of the originator's state rather
+than a shallow copy. Changes made to the original object should not affect the
+state stored in the Memento.
+*/
+void deeptCopyTesting() 
+{
+
+}
+
+/*
+Attempt to restore the originator's state from an invalid or corrupted Memento.
+The pattern should gracefully handle such situations and avoid crashing or
+causing undefined behavior.
+*/
+void invalidStateTesting() 
+{
+
+}
+
+/*
+Try restoring the originator's state partially (e.g., only restoring certain
+attributes or fields). Ensure that the Memento pattern handles partial
+restoration correctly and maintains consistency.
+*/
+void partialRestorationTesting() 
+{
+
+}
+
+void finalTesting() 
+{
+
 }
