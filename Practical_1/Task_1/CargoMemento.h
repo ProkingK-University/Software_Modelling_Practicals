bf209e3e520adbf342626002bda0d7c44dbb0278
Ashley Kapaso <ashleykapaso@gmail.com>
2023-08-04 15:23:40 +0200

    fix merge conflict

--- Practical_1/Task_1/CargoMemento.h
+++ Practical_1/Task_1/CargoMemento.h
@@ -11,6 +11,8 @@ private:
     friend class CargoShip;
 
 public:
+    CargoMemento::CargoMemento(std::string shipName, int shipId, int capacity, int currentLoad, int version, std::string* items);
+    
     std::string* getItems();
 };
 
