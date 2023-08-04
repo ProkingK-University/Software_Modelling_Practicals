f3780fa9493583f949700fa0911600b25eff6200
Ashley Kapaso <ashleykapaso@gmail.com>
2023-08-04 14:47:03 +0200

    add military files and impleament military ship

---
+++ Practical_1/Task_1/MilitaryShip.h
@@ -0,0 +1,38 @@
+#ifndef MILITARYSHIP_H
+#define MILITARYSHIP_H
+
+#include <iostream>
+
+#include "Ship.h"
+#include "MilitaryMemento.h"
+
+class MilitaryShip : public Ship
+{
+public:
+    int capacity;
+    int currentLoad;
+    std::string* weapons;
+
+    MilitaryShip(const MilitaryShip& MilitaryShip);
+    MilitaryShip(int id, std::string name, int capacity);
+
+    int getCapacity();
+    int getCurrentLoad();
+    std::string* getWeapons();
+
+    void setCapacity(int capacity);
+    void setCurrentLoad(int currentLoad);
+    void setWeapons(std::string* weapons, int currentSize, int size);
+
+    void addWeapon(std::string weapon);
+    void removeWeapon(std::string weapon);
+
+    virtual Ship* clone();
+    std::string toString();
+    MilitaryMemento save(int saveId);
+    void restore(MilitaryMemento restorePoint);
+
+    ~MilitaryShip();
+};
+
+#endif
