4bcd144111c4d94f986005f55f54fc6267ac7dc6
Ashley Kapaso <ashleykapaso@gmail.com>
2023-08-08 16:10:14 +0200

    impleament consumer functions

--- Practical_1/Task_2/Consumer.h
+++ Practical_1/Task_2/Consumer.h
@@ -2,11 +2,15 @@
 #define CONSUMER_H
 
 #include <string>
+#include <vector>
 
 class Consumer
 {
 protected:
+    void indent(std::string &result, int level);
     virtual std::string parseData(std::string data) = 0;
+    std::vector<std::string> createVector(std::string data);
+
 public:
     std::string printData(std::string data);
 };
