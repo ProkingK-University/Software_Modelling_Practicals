eef3f18f4bfc6314de69770f0c03073ce87eb09a
Ashley Kapaso <ashleykapaso@gmail.com>
2023-08-08 17:08:56 +0200

    update main

--- Practical_1/Task_2/main.cpp
+++ Practical_1/Task_2/main.cpp
@@ -30,8 +30,12 @@ int main()
     creator.setConsumer("JSON");
     Consumer *json = creator.createConsumer();
 
+    std::cout << "==========Printing XML Output==========" <<std::endl;
     std::cout << xml->printData(xmlData);
+
     std::cout << std::endl;
+
+    std::cout << "==========Printing JSON Output==========" <<std::endl;
     std::cout << json->printData(jsonData);
 
     return 0;
