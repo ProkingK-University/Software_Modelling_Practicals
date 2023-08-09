50198f8856d4a0143cb0a6cd88e6975948c2e959
Joshua Wereley <joshuawereley@icloud.com>
2023-08-09 20:45:58 +0200

    Update JSONConsumer.h
    Removed the virtual keyword from the two functions

--- Practical_1/Task_2/JSONConsumer.h
+++ Practical_1/Task_2/JSONConsumer.h
@@ -6,10 +6,10 @@
 class JSONConsumer : public Consumer
 {
 private:
-    virtual void buildString(const std::vector<std::string> &lines, int &index, std::string &result);
+    void buildString(const std::vector<std::string> &lines, int &index, std::string &result);
 
 public:
-    virtual std::string parseData(std::string data);
+    std::string parseData(std::string data);
 };
 
 #endif
