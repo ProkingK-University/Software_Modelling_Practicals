7e5dacc6db86acd28a7bfb4ae240240229fd9dbb
Ashley Kapaso <ashleykapaso@gmail.com>
2023-08-09 18:09:38 +0200

    format main

--- Practical_1/Task_2/main.cpp
+++ Practical_1/Task_2/main.cpp
@@ -2,34 +2,8 @@
 
 int main()
 {
-/*
-    std::string xmlData = R"(
-<section>
-    cars
-    <section>
-        bmw
-        audi
-    </section>
-    buses
-</section>
-)";
-*/
-
-std::string jsonData = "{\n\tCOS\n\t{\n\t\t110\n\t\t212\n\t}\n\tWTW\n}";
-std::string xmlData = "<section>\n\tcars\n\t<section>\n\t\tbmw\n\t\taudi\n\t</section>\n\tbuses\n</section>";
-
-/*
-    std::string jsonData = R"(
-{
-    COS
-    {
-        110
-        212
-    }
-    WTW
-}
-)";
-*/
+    std::string jsonData = "{\n\tCOS\n\t{\n\t\t110\n\t\t212\n\t}\n\tWTW\n}";
+    std::string xmlData = "<section>\n\tcars\n\t<section>\n\t\tbmw\n\t\taudi\n\t</section>\n\tbuses\n</section>";
 
     std::cout << xmlData <<std::endl;
     std::cout << jsonData <<std::endl;
