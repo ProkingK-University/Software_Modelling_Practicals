8e66b0da82a8f081b6cf8ab9cf64e95d92ccda20
Joshua Wereley <joshuawereley@icloud.com>
2023-08-11 22:36:20 +0200

    Updated main.cpp
    Extended the jsondata and odata

--- Practical_1/Task_2/main.cpp
+++ Practical_1/Task_2/main.cpp
@@ -20,12 +20,13 @@ int main()
 void testingJSONConsumer()
 {
     std::cout << "======================= Test JSON Consumer =======================" << std::endl;
-    std::string jsonData = "{\n\tCOS\n\t{\n\t\t110\n\t\t212\n\t}\n\tWTW\n}";
+    std::string jsonData = "{\n\tName: John\n\tAge:21\n\tIs_Student: true\n\tSubjects{\n\t\tCOS\n\t\t{\n\t\t\t214\n\t\t\t226\n\t\t}\n\t\tWTW\n\t\t{\n\t\t\t285\n\t\t}\n\t}\n}";
     std::cout << jsonData << std::endl;
     ConsumerCreator creator("JSON");
     Consumer* jsonConsumer = creator.createConsumer();
-    if (jsonConsumer) {
-        std::cout << "JSON Consumer Output:" << std::endl;
+    if (jsonConsumer) 
+    {
+        std::cout << std::endl << "JSON Consumer Output:" << std::endl;
         std::string jsonOutput = jsonConsumer->printData(jsonData);
         std::cout << jsonOutput << std::endl;
         delete jsonConsumer;
@@ -36,12 +37,13 @@ void testingJSONConsumer()
 void testingXMLConsumer()
 {
     std::cout << "======================= Test XML Consumer =======================" << std::endl;
-    std::string xmlData = "<section>\n\tcars\n\t<section>\n\t\tbmw\n\t\taudi\n\t</section>\n\tbuses\n</section>";
+    std::string xmlData = "<section>\n\tcars\n\t<section>\n\t\tbmw\n\t\t<section>\n\t\t\tM2\n\t\t\tM3\n\t\t<section>\n\t\taudi\n<section>\n\t\t\tRS3\n\t\t\tRS5\n\t\t<section>\n\tbuses\n</section>";
     std::cout << xmlData << std::endl;
-    ConsumerCreator creator("XML");
+    ConsumerCreator creator("OData");
     Consumer* odataConsumer = creator.createConsumer();
-    if (odataConsumer) {
-        std::cout << "OData Consumer Output:" << std::endl;
+    if (odataConsumer) 
+    {
+        std::cout << std::endl << "OData Consumer Output:" << std::endl;
         std::string odataOutput = odataConsumer->printData(xmlData);
         std::cout << odataOutput << std::endl;
         delete odataConsumer;
