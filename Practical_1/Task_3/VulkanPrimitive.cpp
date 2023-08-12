f3767fd4a0e3f28001c3587e519aecdf8ca4c409
Joshua Wereley <joshuawereley@icloud.com>
2023-08-12 13:29:11 +0200

    Updated VulkanLPrimitive files

---
+++ Practical_1/Task_3/VulkanPrimitive.cpp
@@ -0,0 +1,21 @@
+#include "VulkanPrimitive.h"
+
+float VulkanPrimitive::getXCord()
+{
+    return xcord;
+}
+
+float VulkanPrimitive::getYCord()
+{
+    return ycord;
+}
+
+void VulkanPrimitive::setXCord(float xcord)
+{
+    this->xcord = xcord;
+}
+
+void VulkanPrimitive::setYCord(float ycord)
+{
+    this->ycord = ycord;
+}
---
+++ Practical_1/Task_3/VulkanPrimitive.h
@@ -0,0 +1,19 @@
+#ifndef VULKANPRIMITIVE_H
+#define VULKANPRIMITIVE_H
+
+#include "Primitive.h"
+
+class VulkanPrimitive : public Primitive
+{
+    public:
+        float xcord;
+        float ycord;
+
+        float getXCord();
+        float getYCord();
+
+        void setXCord(float xcord);
+        void setYCord(float ycord);
+};
+
+#endif
