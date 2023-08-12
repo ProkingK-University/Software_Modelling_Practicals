2536a61186942170c85d80d408dac80c0e71ccf5
Joshua Wereley <joshuawereley@icloud.com>
2023-08-12 13:58:43 +0200

    Updated Factory.h & Removed Factory.cpp
    Updated Factory.h to include the pure virtual functions. Removed Factory.cpp because there is no implementation code (All the code will be implemented in the derived classes

--- Practical_1/Task_3/Factory.h
+++ Practical_1/Task_3/Factory.h
@@ -1,6 +1,14 @@
 #ifndef FACTORY_H
 #define FACTORY_H
 
-class Factory
+#include "Window.h"
+#include "Primitive.h"
+
+class Factory 
+{
+    public:
+        virtual Window* createWindow() = 0;
+        virtual Primitive* createPrimitive() = 0
+};
 
 #endif
