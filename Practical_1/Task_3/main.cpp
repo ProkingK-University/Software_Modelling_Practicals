7339892514af0ae2657034e0b40cb6539278cdc3
Joshua Wereley <joshuawereley@icloud.com>
2023-08-12 17:33:52 +0200

    Updated main.cpp

---
+++ Practical_1/Task_3/main.cpp
@@ -0,0 +1,18 @@
+#include "Factory.h"
+#include "OpenGLFactory.h"
+#include "OpenGLPrimitive.h"
+#include "OpenGLWindow.h"
+#include "Primitive.h"
+#include "VulkanFactory.h"
+#include "VulkanPrimitive.h"
+#include "VulkanWindow.h"
+#include "Window.h"
+
+void testingFundamentals();
+void testingRunTimeConfigurability();
+
+int main()
+{
+
+    return 0;
+}
