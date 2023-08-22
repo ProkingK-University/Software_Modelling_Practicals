b46548848d9f383cf7e6a5d1595b910009bc1370
Joshua Wereley <joshuawereley@icloud.com>
2023-08-22 13:39:53 +0200

    Updated Individual.cpp

--- Practical_2/Task_2/Individual.cpp
+++ Practical_2/Task_2/Individual.cpp
@@ -2,17 +2,19 @@
 
 Individual::Individual(int uniqueID, double costCenter, double costToCompany)
 {
-
+    this->uniqueID = uniqueID;
+    this->costCenter = costCenter;
+    this->costToCompany = costToCompany;
 }
 
 double Individual::getTotalExpenditure()
 {
-
+    return costToCompany;
 }
 
 double Individual::getExpenditureByCostCenter()
 {
-
+    return costCenter;
 }
 
 void Individual::add(Organisation* organisation)
