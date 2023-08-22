d7a00cc252add83498b9d2c8903024d030a81478
Joshua Wereley <joshuawereley@icloud.com>
2023-08-22 13:56:07 +0200

    Updated main.cpp in Task 1

--- Practical_2/Task_1/main.cpp
+++ Practical_2/Task_1/main.cpp
@@ -1,6 +1,36 @@
+#include "Accepted.h"
+#include "Completed.h"
+#include "Negotiation.h"
+#include "Rejected.h"
+#include "SmartContract.h"
+#include "SmartState.h"
+#include "Tentatively.h"
+
+#include <iostream>
+
+void testingHandlingUnexpectedStateTransitions();
+void stateInitialisation();
+void dynamicStateChanges();
 
 int main()
 {
-    
+    testingHandlingUnexpectedStateTransitions();
+    stateInitialisation();
+    dynamicStateChanges();
     return 0;
+}
+
+void testingHandlingUnexpectedStateTransitions()
+{
+
+}
+
+void stateInitialisation()
+{
+
+}
+
+void dynamicStateChanges()
+{
+
 }
