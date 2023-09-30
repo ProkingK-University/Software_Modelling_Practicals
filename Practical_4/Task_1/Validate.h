b40ced05b789036c70eeecb7f766cedba61b1df8
Joshua Wereley <joshuawereley@icloud.com>
2023-09-30 23:08:21 +0200

    Added doxygent to Task_1

--- Practical_4/Task_1/Validate.h
+++ Practical_4/Task_1/Validate.h
@@ -3,12 +3,24 @@
 
 #include "Handler.h";
 
+/**
+ * @brief The Validate class for validating requests.
+ */
 class Validate : public Handler
 {
 public:
+    /**
+     * @brief Handle the request validation.
+     */
     void handleRequest();
+
+    /**
+     * @brief Constructor for Validate class.
+     *
+     * @param authorized The next handler for authorized requests.
+     * @param user User associated with the request.
+     */
     Validate(Handler* authorized, User* user) : Handler(authorized, user) {}
-private:
 };
 
 #endif
